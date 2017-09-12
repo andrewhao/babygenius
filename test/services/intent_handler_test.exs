9a7a28de2644006c25640ca3c3f44cde4a65b9e2
Andrew Hao <andrew@carbonfive.com>
2017-09-12 14:09:58 -0700

    Adding get day intent

--- test/services/intent_handler_test.exs
+++ test/services/intent_handler_test.exs
@@ -1,9 +1,52 @@
 defmodule Babygenius.IntentHandlerTest do
   use Babygenius.ModelCase
   use Timex
+  import Babygenius.Factory
   alias Babygenius.{User, DiaperChange, IntentHandler}
 
-  describe "handle_intent/2 for DiaperChange" do
+  describe "handle_intent/3 for GetLastDiaperChange" do
+    setup do
+      amazon_id = "amzn1.ask.account.SOME_ID"
+      user = insert(:user, amazon_id: amazon_id)
+      request_double = %{
+        session: %{
+          user: %{
+            userId: amazon_id
+          }
+        },
+        request: %{
+          intent: %{
+            slots: %{
+            }
+          }
+        }
+      }
+      %{request: request_double, user: user}
+    end
+
+    test "informs about no diaper changes", context do
+      response = IntentHandler.handle_intent("GetLastDiaperChange", context.request)
+      assert response.speak_text == "You have not logged any diaper changes yet"
+    end
+
+    test "it reports a DiaperChange", context do
+      now = Timex.now() |> Timex.set([hour: 7, minute: 12])
+      diaper_change = insert(:diaper_change, occurred_at: now, user: context.user)
+      response = IntentHandler.handle_intent("GetLastDiaperChange", context.request)
+      assert response.speak_text == "The last diaper change occurred today at 7:12 AM"
+    end
+
+    test "it reports the latest DiaperChange", context do
+      time_1 = Timex.now() |> Timex.set([year: 2017, month: 12, day: 25, hour: 12, minute: 0])
+      time_2 = time_1 |> Timex.shift(minutes: 30)
+      insert(:diaper_change, occurred_at: time_1, user: context.user)
+      insert(:diaper_change, occurred_at: time_2, user: context.user)
+      response = IntentHandler.handle_intent("GetLastDiaperChange", context.request)
+      assert response.speak_text == "The last diaper change occurred December 25th at 12:30 PM"
+    end
+  end
+
+  describe "handle_intent/2 for AddDiaperChange" do
     setup do
       request_double = %{
         session: %{
--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,10 +1,21 @@
 defmodule Babygenius.Factory do
-  use ExMachina.Ecto, repo: AppPrototyp.Repo
+  use ExMachina.Ecto, repo: Babygenius.Repo
+  use Timex
+
+  alias Babygenius.{User,DiaperChange}
+
   # Sample user factory
-  # def user_factory do
-  #   %User{
-  #     name: "Test User 1",
-  #     email: sequence(:email, &"testuser#{&1}@example.com"),
-  #   }
-  # end
+  def user_factory do
+    %User{
+      amazon_id: sequence(:email, &"testuser#{&1}@example.com")
+    }
+  end
+
+  def diaper_change_factory do
+    %DiaperChange{
+      user: build(:user),
+      type: "wet",
+      occurred_at: Timex.now()
+    }
+  end
 end
