83eb6132e1963432e9079a0fd8e177ab588c6981
Andrew Hao <andrew@carbonfive.com>
2017-09-12 17:40:53 -0700

    Test env variable output

---
+++ test/integration/test_skills_kit.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo "--- Starting integration test ---"
+COMMAND='ask baby genius when was my last diaper change'
+if ./node_modules/.bin/ask simulate -t "$COMMAND" -l "en-US" -s $ALEXA_APPLICATION_ID | grep -q 'GetLastDiaperChange'; then
+  echo "...OK"
+  echo "Passed $COMMAND"
+else
+  echo "...Failed."
+  echo "Failed $COMMAND"
+  exit 1
+fi
+
+COMMAND='ask baby genius to log a wet diaper at 3 PM'
+if ./node_modules/.bin/ask simulate -t "$COMMAND" -l "en-US" -s $ALEXA_APPLICATION_ID | grep -q 'AddDiaperChange'; then
+  echo "...OK"
+  echo "Passed $COMMAND"
+else
+  echo "...Failed."
+  echo "Failed $COMMAND"
+  exit 1
+fi
+echo "--- Ended integration test ---"
