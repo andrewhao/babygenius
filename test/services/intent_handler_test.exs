f650150ce0b7ed70f988ba8dd6d14771dcdedf03
Andrew Hao <andrewhao@gmail.com>
2017-12-09 21:36:49 -0800

    Intent handler changes

--- test/services/intent_handler_test.exs
+++ test/services/intent_handler_test.exs
@@ -7,7 +7,8 @@ defmodule Babygenius.IntentHandlerTest do
 
   setup do
     Babygenius.AmazonDeviceService.Mock
-    |> expect(:country_and_zip_code, fn(_device, _consent) -> %{"postalCode" => "91111"} end)
+    |> expect(:country_and_zip_code, fn _device, _consent -> %{"postalCode" => "91111"} end)
+
     {:ok, pass: "pass"}
   end
 
@@ -15,6 +16,7 @@ defmodule Babygenius.IntentHandlerTest do
     setup do
       amazon_id = "amzn1.ask.account.SOME_ID"
       user = insert(:user, amazon_id: amazon_id)
+
       request_double = %{
         session: %{
           user: %{
@@ -23,8 +25,7 @@ defmodule Babygenius.IntentHandlerTest do
         },
         request: %{
           intent: %{
-            slots: %{
-            }
+            slots: %{}
           }
         },
         context: %{
@@ -32,16 +33,18 @@ defmodule Babygenius.IntentHandlerTest do
             apiAccessToken: "eyJ0e",
             application: %{applicationId: "amzn1.ask.skill.1175338a-99af-4093-80ab-3e9922f183f7"},
             device: %{
-              deviceId: "DeviceIdValue",
+              deviceId: "DeviceIdValue"
             },
             user: %{
               permissions: %{
                 consentToken: "ConsentTokenValue"
               },
-              userId: "UserIdValue"}
+              userId: "UserIdValue"
+            }
           }
         }
       }
+
       %{request: request_double, user: user}
     end
 
@@ -51,14 +54,14 @@ defmodule Babygenius.IntentHandlerTest do
     end
 
     test "it reports a DiaperChange", context do
-      now = Timex.now() |> Timex.set([hour: 7, minute: 12])
+      now = Timex.now() |> Timex.set(hour: 7, minute: 12)
       insert(:diaper_change, occurred_at: now, user: context.user)
       response = IntentHandler.handle_intent("GetLastDiaperChange", context.request, Timex.now())
       assert response.speak_text == "The last diaper change occurred today at 7:12 AM"
     end
 
     test "it reports the latest DiaperChange", context do
-      time_1 = Timex.now() |> Timex.set([year: 2017, month: 12, day: 25, hour: 12, minute: 0])
+      time_1 = Timex.now() |> Timex.set(year: 2017, month: 12, day: 25, hour: 12, minute: 0)
       time_2 = time_1 |> Timex.shift(minutes: 30)
       insert(:diaper_change, occurred_at: time_1, user: context.user)
       insert(:diaper_change, occurred_at: time_2, user: context.user)
@@ -91,16 +94,18 @@ defmodule Babygenius.IntentHandlerTest do
             apiAccessToken: "eyJ0e",
             application: %{applicationId: "amzn1.ask.skill.1175338a-99af-4093-80ab-3e9922f183f7"},
             device: %{
-              deviceId: "DeviceIdValue",
+              deviceId: "DeviceIdValue"
             },
             user: %{
               permissions: %{
                 consentToken: "ConsentTokenValue"
               },
-              userId: "UserIdValue"}
+              userId: "UserIdValue"
+            }
           }
         }
       }
+
       %{request: request_double}
     end
 
@@ -121,30 +126,30 @@ defmodule Babygenius.IntentHandlerTest do
     test "it uses the current datetime if one is not given", context do
       current_time = Timex.now()
       IntentHandler.handle_intent("AddDiaperChange", context.request, current_time)
-      diaper_change = DiaperChange |> last |> Repo.one
-      assert diaper_change.occurred_at |> Map.put(:microseconds, 0) == current_time |> Map.put(:microseconds, 0)
+      diaper_change = DiaperChange |> last |> Repo.one()
+
+      assert diaper_change.occurred_at |> Map.put(:microseconds, 0) ==
+               current_time |> Map.put(:microseconds, 0)
     end
 
     test "it uses provided date if one is given", context do
-      request = put_in(
-        context.request,
-        [:request, :intent, :slots, "diaperChangeTime"],
-        %{"value" => "19:00"}
-      ) |> put_in(
-        [:request, :intent, :slots, "diaperChangeDate"],
-        %{"value" => "2017-09-10"}
-      )
+      request =
+        put_in(context.request, [:request, :intent, :slots, "diaperChangeTime"], %{
+          "value" => "19:00"
+        })
+        |> put_in([:request, :intent, :slots, "diaperChangeDate"], %{"value" => "2017-09-10"})
+
       current_time = Timex.now()
       response = IntentHandler.handle_intent("AddDiaperChange", request, current_time)
       assert response.speak_text == "A wet diaper change was logged September 10th at 7:00 PM"
     end
 
     test "it uses the provided time if one is given", context do
-      request = put_in(
-        context.request,
-        [:request, :intent, :slots, "diaperChangeTime"],
-        %{"value" => "19:00"}
-      )
+      request =
+        put_in(context.request, [:request, :intent, :slots, "diaperChangeTime"], %{
+          "value" => "19:00"
+        })
+
       current_time = Timex.now() |> Timex.set(hour: 12, minute: 0, second: 0)
       response = IntentHandler.handle_intent("AddDiaperChange", request, current_time)
       assert response.speak_text == "A wet diaper change was logged today at 7:00 PM"
--- web/services/intent_handler.ex
+++ web/services/intent_handler.ex
@@ -13,13 +13,18 @@ defmodule Babygenius.IntentHandler do
 
     {:ok, value} = FetchTimezoneData.perform(user.id, request)
 
-    diaper_change = from(d in DiaperChange, where: d.user_id == ^user.id, order_by: d.occurred_at) |> last |> Repo.one
-    speak_text = case diaper_change do
-      nil ->
-        "You have not logged any diaper changes yet"
-      _ ->
-        "The last diaper change occurred #{formatted_time(diaper_change.occurred_at)}"
-    end
+    diaper_change =
+      from(d in DiaperChange, where: d.user_id == ^user.id, order_by: d.occurred_at) |> last
+      |> Repo.one()
+
+    speak_text =
+      case diaper_change do
+        nil ->
+          "You have not logged any diaper changes yet"
+
+        _ ->
+          "The last diaper change occurred #{formatted_time(diaper_change.occurred_at)}"
+      end
 
     %{speak_text: speak_text, should_end_session: true}
   end
@@ -33,19 +38,33 @@ defmodule Babygenius.IntentHandler do
     slots = request.request.intent.slots
     diaper_type = get_in(slots, ["diaperType", "value"])
     fetched_diaper_change_date = get_in(slots, ["diaperChangeDate", "value"])
-    diaper_change_date = case fetched_diaper_change_date do
-      nil -> now
-      _ ->
-        Timex.parse!(fetched_diaper_change_date, "%Y-%m-%d", :strftime)
-    end
+
+    diaper_change_date =
+      case fetched_diaper_change_date do
+        nil ->
+          now
+
+        _ ->
+          Timex.parse!(fetched_diaper_change_date, "%Y-%m-%d", :strftime)
+      end
 
     fetched_diaper_change_time = get_in(slots, ["diaperChangeTime", "value"])
-    diaper_change_time = case fetched_diaper_change_time do
-      nil -> now
-      _ ->
-        diaper_change_date_formatted = diaper_change_date |> Timex.format!("%Y-%m-%d", :strftime)
-        Timex.parse!("#{diaper_change_date_formatted} #{fetched_diaper_change_time}", "%Y-%m-%d %H:%M", :strftime)
-    end
+
+    diaper_change_time =
+      case fetched_diaper_change_time do
+        nil ->
+          now
+
+        _ ->
+          diaper_change_date_formatted =
+            diaper_change_date |> Timex.format!("%Y-%m-%d", :strftime)
+
+          Timex.parse!(
+            "#{diaper_change_date_formatted} #{fetched_diaper_change_time}",
+            "%Y-%m-%d %H:%M",
+            :strftime
+          )
+      end
 
     %DiaperChange{user_id: user.id, type: diaper_type, occurred_at: diaper_change_time}
   end
@@ -55,23 +74,29 @@ defmodule Babygenius.IntentHandler do
     {:ok, value} = FetchTimezoneData.perform(user.id, request)
 
     diaper_change_from_request(user, request, now)
-    |> Repo.insert!
+    |> Repo.insert!()
     |> diaper_change_speech
   end
 
   defp diaper_change_speech(diaper_change) do
-    speak_text = "A #{diaper_change.type} diaper change was logged #{formatted_time(diaper_change.occurred_at)}"
+    speak_text =
+      "A #{diaper_change.type} diaper change was logged #{
+        formatted_time(diaper_change.occurred_at)
+      }"
+
     %{speak_text: speak_text, should_end_session: true}
   end
 
   defp formatted_time(datetime) do
     now = Timex.now()
-    speak_date = if now.day == datetime.day && now.month == datetime.month do
-      "today"
-    else
-      day = datetime.day
-      "#{Timex.format!(datetime, "%B", :strftime)} #{day}#{ordinal(day)}"
-    end
+
+    speak_date =
+      if now.day == datetime.day && now.month == datetime.month do
+        "today"
+      else
+        day = datetime.day
+        "#{Timex.format!(datetime, "%B", :strftime)} #{day}#{ordinal(day)}"
+      end
 
     speak_time = datetime |> Timex.format!("%-I:%M %p", :strftime)
 
@@ -82,10 +107,18 @@ defmodule Babygenius.IntentHandler do
     cond do
       Enum.any?([11, 12, 13], &(&1 == Integer.mod(num, 100))) ->
         "th"
-      Integer.mod(num, 10) == 1 -> "st"
-      Integer.mod(num, 10) == 2 -> "nd"
-      Integer.mod(num, 10) == 3 -> "rd"
-      true -> "th"
+
+      Integer.mod(num, 10) == 1 ->
+        "st"
+
+      Integer.mod(num, 10) == 2 ->
+        "nd"
+
+      Integer.mod(num, 10) == 3 ->
+        "rd"
+
+      true ->
+        "th"
     end
   end
 end
