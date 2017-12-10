5218c91daa58dada1cd77db3958f472b190c2fcf
Andrew Hao <andrewhao@gmail.com>
2017-12-09 22:13:43 -0800

    Fixing microsecond problem

--- test/services/intent_handler_test.exs
+++ test/services/intent_handler_test.exs
@@ -128,8 +128,8 @@ defmodule Babygenius.IntentHandlerTest do
       IntentHandler.handle_intent("AddDiaperChange", context.request, current_time)
       diaper_change = DiaperChange |> last |> Repo.one()
 
-      assert diaper_change.occurred_at |> Map.put(:microseconds, 0) ==
-               current_time |> Map.put(:microseconds, 0)
+      assert diaper_change.occurred_at |> Map.put(:microsecond, 0) ==
+               current_time |> Map.put(:microsecond, 0)
     end
 
     test "it uses provided date if one is given", context do
