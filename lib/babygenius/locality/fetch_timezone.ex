8fe23e67f39bc2cc8c09c8ad10417cc4cbd8663b
Andrew Hao <andrewhao@gmail.com>
2017-12-12 13:58:03 -0800

    FetchTimezone now is a plain service

--- lib/babygenius/locality/fetch_timezone.ex
+++ lib/babygenius/locality/fetch_timezone.ex
@@ -3,14 +3,8 @@ defmodule Babygenius.Locality.FetchTimezone do
   Task that checks the timezone for user in zip, and updates user
   if the timezone is valid.
   """
-  use Task
   alias BabygeniusWeb.User
   alias Babygenius.{Repo, Locality}
-
-  @spec start_link(String.t(), %User{}) :: {:ok, pid}
-  def start_link(zipcode, user) do
-    Task.start_link(__MODULE__, :run, [zipcode, user])
-  end
 
   @spec run(String.t(), %User{}) :: %User{}
   def run(zipcode, user) do
