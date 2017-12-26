07d4904ce1480f82b73600eff3aeb2cb866e4c2f
Andrew Hao <andrew@carbonfive.com>
2017-12-25 20:27:18 -0800

    Adding a more comprehensive suite of test fixtures to the controller test

--- test/controllers/alexa_controller_test.exs
+++ test/controllers/alexa_controller_test.exs
@@ -5,6 +5,7 @@ defmodule Babygenius.AlexaControllerTest do
 
   import Mox
 
+  alias Babygenius.RequestFixtures
   alias BabygeniusWeb.{User, DiaperChange}
 
   setup do
@@ -14,108 +15,79 @@ defmodule Babygenius.AlexaControllerTest do
     {:ok, pass: "pass"}
   end
 
-  describe "intent_request/3" do
+  describe "GetLastDiaperChange intent_request/3" do
     setup do
-      json = """
-      {
-        "session": {
-          "new": false,
-          "sessionId": "SessionId.adb3cd0a-f4fd-46be-acfb-d490b3a9b2d6",
-          "application": {
-            "applicationId": "amzn1.ask.skill.1175338a-99af-4093-80ab-3e9922f183f7"
-          },
-          "attributes": {},
-          "user": {
-            "userId": "amzn1.ask.account.AFBXXFOBZCPX4X7Y42SGPWDWBBUTB56PB2NPD4WLYA7JWSWMDQTVMUI6UA2KZTVT3QJT5CRV7Q3GZVXZQ3VC56IFBG32V5VDMAENXGIZF7QOPI6MRHK3JJHAZS5MWGR3WELRUEIZVRLEPNV6HJLUMJBSOEPOTVER2KHLXZIY27EAAEP5QQSZSLAC6R2DFLR65WXN6E3S6RH4VFY"
-          }
-        },
-        "request": {
-          "type": "IntentRequest",
-          "requestId": "EdwRequestId.f7639b89-fa9b-45cc-8a1b-75da66ee3c8e",
-          "intent": {
-            "name": "AddDiaperChange",
-            "slots": {
-              "diaperChangeDate": {
-                "name": "diaperChangeDate",
-                "value": "2017-09-08"
-              },
-              "diaperChangeTime": {
-                "name": "diaperChangeTime",
-                "value": "03:00"
-              },
-              "logAction": {
-                "name": "logAction",
-                "value": "log"
-              },
-              "diaperType": {
-                "name": "diaperType",
-                "value": "wet"
-              }
-            }
-          },
-          "locale": "en-US",
-          "timestamp": "2017-09-08T01:15:02Z"
-        },
-        "context": {"AudioPlayer": {"playerActivity": "IDLE"},
-          "System": {
-            "apiAccessToken": "eyJ0e",
-            "apiEndpoint": "https://api.amazonalexa.com",
-            "application": {"applicationId": "amzn1.ask.skill.1175338a-99af-4093-80ab-3e9922f183f7"},
-            "device": {
-              "deviceId": "amzn1.ask.device.AH6U6HY6STJGU5EWAJ6PKEPWG5W6QCOQCJZ7JCL6O5R7TQVXOMVM6YDYK6PXXBD6PEGO3NXGPICDRERSICU65VRZZAMAWBETLA6B4O2RV23K6G6EMMUAWLSIV3ONZXNSRVU35GMGJMFWYABJWYLOQM57UMIQ",
-              "supportedInterfaces": {"AudioPlayer": {}}
-            },
-            "user": {
-              "permissions": {
-                "consentToken": "eyJ0eX"
-              },
-              "userId": "amzn1.ask.account.AFBXXFOBZCPX4X7Y42SGPWDWBBUTB56PB2NPD4WLYA7JWSWMDQTVMUI6UA2KZTVT3QJT5CRV7Q3GZVXZQ3VC56IFBG32V5VDMAENXGIZF7QOPI6MRHK3JJHAZS5MWGR3WELRUEIZVRLEPNV6HJLUMJBSOEPOTVER2KHLXZIY27EAAEP5QQSZSLAC6R2DFLR65WXN6E3S6RH4VFY"}
-          }
-        },
-        "version": "1.0"
-      }
-      """
+      user = insert(:user, amazon_id: RequestFixtures.amazon_id())
+
+      # Insert a previous diaper change at 8:00AM today
+      insert(
+        :diaper_change,
+        user: user,
+        occurred_at: Timex.now() |> Timex.set(hour: 8, minute: 5)
+      )
 
       request =
         build_conn()
         |> put_req_header("accept", "application/json")
         |> put_req_header("content-type", "application/json")
 
-      {:ok, request: request, json: json}
+      {:ok, request: request, json: RequestFixtures.get_last_diaper_change_json()}
     end
 
-    test "responds with shouldEndSession true", context do
+    test "it responds with confirmation text", %{request: request, json: json} do
       response =
-        context[:request]
-        |> post(alexa_path(build_conn(), :command), context[:json])
+        request
+        |> post(alexa_path(build_conn(), :command), json)
+        |> json_response(200)
+
+      expected_text = "The last diaper change occurred today at 8:05 AM"
+
+      assert get_in(response, ["response", "outputSpeech", "text"]) == expected_text
+    end
+  end
+
+  describe "AddDiaperChange intent_request/3" do
+    setup do
+      request =
+        build_conn()
+        |> put_req_header("accept", "application/json")
+        |> put_req_header("content-type", "application/json")
+
+      {:ok, request: request, json: RequestFixtures.add_diaper_change_json()}
+    end
+
+    test "responds with shouldEndSession true", %{request: request, json: json} do
+      response =
+        request
+        |> post(alexa_path(build_conn(), :command), json)
         |> json_response(200)
 
       assert response["response"]["shouldEndSession"] == true
     end
 
-    test "adds a DiaperChange record into the DB", context do
+    test "adds a DiaperChange record into the DB", %{request: request, json: json} do
       old_count = Repo.aggregate(from(dc in "diaper_changes"), :count, :id)
 
-      context[:request]
-      |> post(alexa_path(build_conn(), :command), context[:json])
+      request
+      |> post(alexa_path(build_conn(), :command), json)
       |> json_response(200)
 
       new_count = Repo.aggregate(from(dc in "diaper_changes"), :count, :id)
       assert old_count == new_count - 1
     end
 
-    test "it creates a new user if one does not already exist", context do
+    test "it creates a new user if one does not already exist", %{request: request, json: json} do
       old_count = Repo.aggregate(from(dc in "users"), :count, :id)
 
-      context[:request]
-      |> post(alexa_path(build_conn(), :command), context[:json])
+      request
+      |> post(alexa_path(build_conn(), :command), json)
       |> json_response(200)
 
       new_count = Repo.aggregate(from(dc in "users"), :count, :id)
       assert old_count == new_count - 1
     end
 
-    test "it does not create a new user if one already exists", context do
+    test "it does not create a new user if one already exists", %{request: request, json: json} do
       %User{
         amazon_id:
           "amzn1.ask.account.AFBXXFOBZCPX4X7Y42SGPWDWBBUTB56PB2NPD4WLYA7JWSWMDQTVMUI6UA2KZTVT3QJT5CRV7Q3GZVXZQ3VC56IFBG32V5VDMAENXGIZF7QOPI6MRHK3JJHAZS5MWGR3WELRUEIZVRLEPNV6HJLUMJBSOEPOTVER2KHLXZIY27EAAEP5QQSZSLAC6R2DFLR65WXN6E3S6RH4VFY"
@@ -124,32 +96,34 @@ defmodule Babygenius.AlexaControllerTest do
 
       old_count = Repo.aggregate(from(dc in "users"), :count, :id)
 
-      context[:request]
-      |> post(alexa_path(build_conn(), :command), context[:json])
+      request
+      |> post(alexa_path(build_conn(), :command), json)
       |> json_response(200)
 
       new_count = Repo.aggregate(from(dc in "users"), :count, :id)
       assert old_count == new_count
     end
 
-    test "logs a DiaperChange with the right type", context do
+    test "logs a DiaperChange with the right type", %{request: request, json: json} do
       _response =
-        context[:request]
-        |> post(alexa_path(build_conn(), :command), context[:json])
+        request
+        |> post(alexa_path(build_conn(), :command), json)
         |> json_response(200)
 
-      type = DiaperChange |> last |> Repo.one() |> Map.fetch!(:type)
+      type =
+        DiaperChange
+        |> last
+        |> Repo.one()
+        |> Map.fetch!(:type)
 
       assert type == "wet"
     end
 
-    test "it responds with confirmation text", context do
+    test "it responds with confirmation text", %{request: request, json: json} do
       response =
-        context[:request]
-        |> post(alexa_path(build_conn(), :command), context[:json])
+        request
+        |> post(alexa_path(build_conn(), :command), json)
         |> json_response(200)
-
-      DiaperChange |> last |> Repo.one() |> Map.fetch!(:occurred_at)
 
       assert get_in(response, ["response", "outputSpeech", "text"]) ==
                "A wet diaper change was logged September 8th at 3:00 AM"
@@ -158,27 +132,7 @@ defmodule Babygenius.AlexaControllerTest do
 
   describe "launch_request/2" do
     test "responds with initial launch text" do
-      json = """
-      {
-        "session": {
-          "sessionId": "SessionId.80ef8951-172e-4f02-ace8-a7ec847e2d9f",
-          "application": {
-            "applicationId": "amzn1.echo-sdk-ams.app.05dcb1a4-cb45-46c5-a30e-bb3033a0770a"
-          },
-          "attributes": {},
-          "user": {
-            "userId": "amzn1.ask.account.AFP3ZWPOS2BGJR7OWJZ3DHPKMOMNWY4AY66FUR7ILBWANIHQN73QH3G5PC2FJVGDIDA7MY54GGNRGM4SVPKTT3K53SLI232MEFI77TZN7W6LISNFZTTFDSPCLX6OB4ISJDVJB6QZO3XC74US6CH5DQXYCVOODNTUFNI5JNSUWSBDVMWB7JXPVX43P4EUIMHTPZHNRHZDUDENZVI"
-          },
-          "new": false
-        },
-        "request": {
-          "type": "LaunchRequest",
-          "requestId": "EdwRequestId.27142539-8af4-430c-8f22-411cfab269bd",
-          "timestamp": "2016-07-07T00:45:08Z"
-        },
-        "version": "1.0"
-      }
-      """
+      json = RequestFixtures.launch_request_json()
 
       response =
         build_conn()
