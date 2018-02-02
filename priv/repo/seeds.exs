e7e926e6e14ab7f6926e2fc30c2f5ced0999d14d
Andrew Hao <andrew@carbonfive.com>
2018-02-02 11:12:35 -0800

    Add seed

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,41 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Babygenius.{Identity, BabyLife}
+
+IO.puts("Inserting user")
+
+user =
+  %Identity.User{
+    amazon_id: "amz.user.1"
+  }
+  |> Identity.find_or_create_user_by_amazon_id()
+
+IO.puts("Inserting diaper change")
+
+BabyLife.create_diaper_change(
+  user,
+  "wet",
+  "09:00",
+  "2017-01-01",
+  "America/Los_Angeles",
+  DateTime.utc_now()
+)
+
+IO.puts("Inserting feeding")
+
+BabyLife.create_feeding(
+  %{
+    user: user,
+    feed_type: "feeding",
+    volume: 60,
+    unit: "ml",
+    time: "12:00",
+    date: "2017-01-01",
+    user_timezone: "America/Los_Angeles"
+  },
+  DateTime.utc_now()
+)
+
+IO.puts("Done.")
